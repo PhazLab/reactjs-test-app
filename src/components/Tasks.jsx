abf075146266e926a6f1295b3841e03acd53b8e2
Samuel Bouye <bouyesamuel@gmail.com>
2022-06-23 16:39:23 +0000

    change ended to done

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -69,28 +69,38 @@ function Tasks () {
     const allTasksItem = tasks.map((task, index) => {
         return task.status === false && 
             <TaskItem 
-            task={task}
-            i={index}
-            key={index}
-            editMode={edit}
-            editText={editText}
-            editSubmit={(e,todo) => handleEditSubmit(e,todo)}
-            deleteTask={todo => handleDeleteClicked(todo)}
-            editTask={todo => handleEditTaskClick(todo)}
-            changeStatus={handleChangeStatus}
-            editChange={handleEditChange}
-            todoId={todoId}
+                task={task}
+                i={index}
+                key={index}
+                editMode={edit}
+                editText={editText}
+                editSubmit={(e,todo) => handleEditSubmit(e,todo)}
+                deleteTask={todo => handleDeleteClicked(todo)}
+                editTask={todo => handleEditTaskClick(todo)}
+                changeStatus={handleChangeStatus}
+                editChange={handleEditChange}
+                todoId={todoId}
             />
     })
 
     const allTasksDone = tasks.map((task, index) => {
         return task.status === true && 
-            <TaskItem task={task} i={index} key={index} deleteTask={todo => handleDeleteClicked(todo)}  changeStatus={handleChangeStatus} />
+            <TaskItem 
+                task={task}
+                i={index} 
+                key={index}
+                deleteTask={todo => handleDeleteClicked(todo)}
+                changeStatus={handleChangeStatus}
+            />
     })
     
 return <>
         {tasks.length} Tasks
-        <TaskForm change={handleChange} submit={handleSubmit} text={text}/>
+        <TaskForm 
+            change={handleChange}
+            submit={handleSubmit}
+            text={text}
+        />
         
         <h3>All Tasks</h3>
         <ul>
@@ -98,7 +108,7 @@ return <>
         </ul>
 
         <hr />
-        <h3>All Tasks Ended</h3>
+        <h3>All Tasks Done</h3>
         <ul>
             {allTasksDone}
         </ul>
